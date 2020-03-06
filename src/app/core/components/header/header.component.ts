9984144766a33646101ffc77583b03470eb1cb78
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2020-03-07 01:37:49 +0300

    feat: add header component refactor: add some general style in styles.scss

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  public isMenuCollapsed = true;
+  public language: string;
+
+  constructor(private router: Router) {}
+
+  public ngOnInit(): void {
+    this.language = 'EN';
+  }
+
+  public goTo(url): void {
+    this.router.navigateByUrl(url);
+  }
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared/shared.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [CommonModule, SharedModule, NgbModule],
-  exports: [SharedModule, NgbModule]
+  exports: [SharedModule, HeaderComponent]
 })
 export class CoreModule {}
