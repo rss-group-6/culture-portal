9aa764bb23f4bc2b2b31dab541ab808f5c4c986f
ekater1na <ekaterina_l@tut.by>
2020-03-07 12:05:10 +0300

    feat: add developers info

---
+++ src/app/developer.ts
@@ -0,0 +1,8 @@
+export interface Developer {
+  name: string,
+  surname: string,
+  photo: string,
+  github: string,
+  telegram: string,
+  worklog: string
+}
---
+++ src/app/mock.developers.ts
@@ -0,0 +1,59 @@
+import { Developer } from './developer';
+
+import kravchenko from '../assets/images/developers/kravchenko.jfif';
+import lysiuk from '../assets/images/developers/lysiuk.png';
+import sidarkevich from '../assets/images/developers/sidarkevich.png';
+import shcheglakov from '../assets/images/developers/shcheglakov.png';
+import golomb from '../assets/images/developers/golomb.png';
+import popova from '../assets/images/developers/popova.jfif';
+
+export const Developers: Developer[] = [
+{
+  name: 'Evgeny',
+  surname: "Kravchenko",
+  photo: kravchenko,
+  github: 'https://github.com/Evgeny-Kravchenko',
+  telegram: 'https://t.me/k1696',
+  worklog: '10%'
+},
+{
+  name: 'Ekaterina',
+  surname: "Lysiuk",
+  photo: lysiuk,
+  github: 'https://github.com/ekater1na',
+  telegram: 'https://t.me/ObsssQ',
+  worklog: '10%'
+},
+{
+  name: 'Mikhail',
+  surname: "Sidarkevich",
+  photo: sidarkevich,
+  github: 'https://github.com/chivekrodis',
+  telegram: 'string',
+  worklog: '10%'
+},
+{
+  name: 'Mikhail',
+  surname: "Shcheglakov",
+  photo: shcheglakov,
+  github: 'https://github.com/madmike85',
+  telegram: 'https://t.me/MikhailShcheglakov',
+  worklog: '10%'
+},
+{
+  name: 'Maria',
+  surname: "Golomb",
+  photo: golomb,
+  github: 'https://github.com/mariagolomb',
+  telegram: 'https://t.me/MariaGolomb',
+  worklog: '10%'
+},
+{
+  name: 'Anastasiya',
+  surname: "Popova",
+  photo: popova,
+  github: 'https://github.com/a-popova',
+  telegram: 'https://t.me/anastasiya2810',
+  worklog: '10%'
+},
+]
