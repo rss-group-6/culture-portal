1abc9907e479ad65b1db8921f6fc3ae948f44beb
Mikhail Sidarkevich <chivekrodis@gmail.com>
2020-03-08 12:24:35 +0300

    feat: add path to component

--- src/app/worklog/worklog-routing.module.ts
+++ src/app/worklog/worklog-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WorklogComponent } from './components/worklog/worklog.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: WorklogComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
