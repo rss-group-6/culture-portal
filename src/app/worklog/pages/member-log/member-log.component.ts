0b2eef763c46a38537b4551684a2b917815d31a3
Mikhail Sidarkevich <chivekrodis@gmail.com>
2020-03-08 18:02:54 +0300

    feat: update files for work new log data

--- src/app/worklog/pages/member-log/member-log.component.ts
+++ src/app/worklog/pages/member-log/member-log.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GeneralLog } from '../../models/worklog.models';
 import { WorklogService } from '../../services/worklog.service';
 
@@ -9,32 +9,47 @@ import { WorklogService } from '../../services/worklog.service';
 })
 export class MemberLogComponent {
   public logs = GeneralLog;
+  public generalDifficulties =
+    GeneralLog[this.worklogService.lang][this.worklogService.counter].difficultiesLn;
 
   constructor(public worklogService: WorklogService) {}
 
   public findItem(id: number): void {
-    this.logs[this.worklogService.counter].item[id - 1].completed = !this.logs[this.worklogService.counter]
-      .item[id - 1].completed;
+    this.logs[this.worklogService.lang][this.worklogService.counter].item[id - 1].completed = !this.logs[
+      this.worklogService.lang
+    ][this.worklogService.counter].item[id - 1].completed;
   }
 
-  public calculateTotal(): string {
+  public calculateTotal(lang: string): string {
     let acc = 0;
     let total: string;
 
     if (this.worklogService.counter === 0) {
-      this.logs[this.worklogService.counter].item.map(e => {
+      this.logs[lang][this.worklogService.counter].item.map(e => {
         if (e.completed) {
           acc += +e.points;
         }
       });
-      total = `Total: ${acc} pt`;
-    } else {
-      this.logs[this.worklogService.counter].item.map(e => {
+    }
+
+    if (this.worklogService.counter) {
+      this.logs[lang][this.worklogService.counter].item.map(e => {
         if (e.completed) {
           acc += +e.points.slice(0, -3);
         }
       });
-      total = `Total: ${acc} hr`;
+    }
+
+    switch (lang) {
+      case 'RU':
+        !this.worklogService.counter ? (total = `Всего: ${acc} баллов`) : (total = `Всего: ${acc} часов`);
+        break;
+      case 'EN':
+        !this.worklogService.counter ? (total = `Total: ${acc} pt`) : (total = `Total: ${acc} hr`);
+        break;
+      case 'BY':
+        !this.worklogService.counter ? (total = `Усяго: ${acc} балаў`) : (total = `Усяго: ${acc} гадзін(ы)`);
+        break;
     }
 
     return total;
