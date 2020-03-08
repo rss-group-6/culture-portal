970d335254244ca5e4fa38c9433e359961c06c6d
Mikhail Sidarkevich <chivekrodis@gmail.com>
2020-03-08 18:04:31 +0300

    refactor: remove unneccessary code

--- src/app/worklog/components/worklog/worklog.component.ts
+++ src/app/worklog/components/worklog/worklog.component.ts
@@ -3,13 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-worklog',
   templateUrl: './worklog.component.html',
-  styleUrls: ['./worklog.component.scss']
+  styleUrls: ['./worklog.component.scss'],
 })
-export class WorklogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class WorklogComponent {}
