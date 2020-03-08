f1b11bc3ad93b6e43c6f40b78ac8af29bca2f6e3
Anastasiya Popova <Anastasiya@Anastasiyas-MacBook-Pro.local>
2020-03-08 22:20:53 +0300

    feat: move author-of-day to coreModule

--- src/app/directors/directors-routing.module.ts
+++ src/app/directors/directors-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DirectorOfDayPageComponent } from './pages/director-of-day-page/director-of-day-page.component';
 
-const routes: Routes = [
-  { path: '', component: DirectorOfDayPageComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/directors/directors.module.ts
+++ src/app/directors/directors.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DirectorsRoutingModule } from './directors-routing.module';
-import { DirectorOfDayComponent } from './components/director-of-day/director-of-day.component';
-import { DirectorOfDayPageComponent } from './pages/director-of-day-page/director-of-day-page.component';
 
 @NgModule({
-  declarations: [DirectorOfDayComponent, DirectorOfDayPageComponent],
+  declarations: [],
   imports: [CommonModule, DirectorsRoutingModule],
 })
 export class DirectorsModule {}
