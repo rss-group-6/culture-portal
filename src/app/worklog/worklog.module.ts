630061e406c1aafec15bc9aef2824e477f3f3dea
Mikhail Sidarkevich <chivekrodis@gmail.com>
2020-03-08 13:34:35 +0300

    refactor: remove comments

--- src/app/worklog/worklog.module.ts
+++ src/app/worklog/worklog.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { WorkLogRoutingModule } from './worklog-routing.module';
 import { WorklogComponent } from './components/worklog/worklog.component';
 import { MemberLogComponent } from './pages/member-log/member-log.component';
 import { CarouselNavigationComponent } from './components/carousel-navigation/carousel-navigation.component';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 @NgModule({
   declarations: [WorklogComponent, MemberLogComponent, CarouselNavigationComponent],
   imports: [CommonModule, WorkLogRoutingModule, NgbModule],
-  // exports: [WorklogComponent, MemberLogComponent, CarouselNavigationComponent],
 })
 export class WorkLogModule {}
