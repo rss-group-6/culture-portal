66053f3c9fdadebc460c6e636e6d2b09eeff942a
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2020-03-10 19:26:28 +0300

    fix: add error with BE.json

--- src/app/team-members/team-members.module.ts
+++ src/app/team-members/team-members.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TeamMembersRoutingModule } from './team-members-routing.module';
 import { DeveloperItemComponent } from './components/developer-item/developer-item.component';
 import { DevelopersListComponent } from './pages/developers-list/developers-list.component';
