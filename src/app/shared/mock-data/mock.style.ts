0af5eef8e280c6cb90429cbf0be53ea64a20a55e
ekater1na <ekaterina_l@tut.by>
2020-03-13 01:01:33 +0300

    feat: add mock-data

---
+++ src/app/shared/mock-data/mock.style.ts
@@ -0,0 +1,32 @@
+import { StyleItem } from '@shared/models/style'
+
+export const StyleItems: StyleItem[] = [
+  {
+    "name": "Colors",
+    "content": "content"
+  },
+  {
+    "name": "Typography",
+    "content": "content"
+  },
+  {
+    "name": "Inputs",
+    "content": "content"
+  },
+  {
+    "name": "Buttons",
+    "content": "content"
+  },
+  {
+    "name": "CheckBox",
+    "content": "content"
+  },
+  {
+    "name": "Table",
+    "content": "content"
+  },
+  {
+    "name": "Cards",
+    "content": "content"
+  }
+]
