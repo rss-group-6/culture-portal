fcda7de52456fe72e155f1d90789b4e7b4d9141a
ekater1na <ekaterina_l@tut.by>
2020-03-13 00:00:38 +0300

    build: add style-list component

---
+++ src/app/styleguide/pages/style-list/style-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-style-list',
+  templateUrl: './style-list.component.html',
+  styleUrls: ['./style-list.component.scss']
+})
+export class StyleListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
