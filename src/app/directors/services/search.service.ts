d4b79d61f60447ec064a48d689b5de95f44fcfab
Maria Golomb <marialukashuk.ml@gmail.com>
2020-03-13 21:46:47 +0300

    feat: update search service structure

--- src/app/directors/services/search.service.ts
+++ src/app/directors/services/search.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Directors } from '@shared/mock-data/mock.directors';
 import { Director } from '@shared/models/director';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { LanguageService } from '@core/services/language.service';
+import { BrowserStack } from 'protractor/built/driverProviders';
 
 @Injectable({
   providedIn: 'root'
@@ -9,23 +11,59 @@ import { BehaviorSubject, Observable } from 'rxjs';
 export class SearchService {
 
   public arrOfDirectors: Director[] = Directors;
-
   public subjectSearch: BehaviorSubject<Director[]>;
   public observeSearch: Observable<Director[]>;
+  public currentLng: string;
+  protected EN = 'EN';
+  protected RU = 'RU';
+  protected BE = 'BE';
 
-  constructor() {
+
+  constructor(private languageService: LanguageService) {
     this.subjectSearch = new BehaviorSubject(this.arrOfDirectors);
     this.observeSearch = this.subjectSearch.asObservable();
+
+
+    this.languageService.getLanguage().subscribe(lang => {
+      this.currentLng = lang;
+      console.log('lng: ' + this.currentLng);
+    });
   }
 
-  public getCurrentLenguage() { }
-
   private searchByQuery(query: string) {
+
     const queryStr: string = query.toLowerCase();
-    return this.arrOfDirectors.filter(director => director.name.toLowerCase().includes(queryStr)
+
+
+
+    switch (this.currentLng) {
+      case this.EN:
+
+
+        break;
+
+      case this.RU:
+        return this.searchIn(this.arrOfDirectors, queryStr);
+
+      case this.BE:
+
+        break;
+
+    }
+
+
+
+
+  }
+
+  private searchIn(arrOfDirectors, queryStr) {
+
+
+    return arrOfDirectors.filter(director => director.name.toLowerCase().includes(queryStr)
       || director.surname.toLowerCase().includes(queryStr)
       || director.birthPlace.toLowerCase().includes(queryStr)
     );
+
   }
 
   public getSearchResult(query: string) {
